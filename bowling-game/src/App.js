51ff467f944a7a278c680cdf893fff55a0e2284a
hugh.burgess <hugh.burgess@check24.de>
2022-04-05 20:33:39 +0200

    first commit

---
+++ bowling-game/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+      <table>
+        <tr>
+          <th>Roll 1</th>
+          <th>Roll 2</th>
+        </tr>
+
+        <tr>
+          Total
+        </tr>
+      </table>
+  );
+}
+
+export default App;
