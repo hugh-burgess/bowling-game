e6f6b00dea8e832b73452703fa0c7bd3666fb14a
hugh.burgess <hugh.burgess@check24.de>
2022-04-05 21:09:49 +0200

    bowling scores set out

--- bowling-game/src/App.js
+++ bowling-game/src/App.js
@@ -1,18 +1,32 @@
 import './App.css';
 
 function App() {
-  return (
-      <table>
-        <tr>
-          <th>Roll 1</th>
-          <th>Roll 2</th>
-        </tr>
 
-        <tr>
-          Total
-        </tr>
-      </table>
-  );
+    const arrayOfRounds = [];
+
+    for (let i = 1; i <= 10; i++) {
+        arrayOfRounds.push(i);
+    }
+    console.log(arrayOfRounds);
+
+
+    const gameRound =
+        <table>
+            <th>
+                {arrayOfRounds.map((item,index)=>{
+                    return (
+                        <th key={index}>{item} | {item}</th>
+                    )
+                })}
+            </th>
+        </table>;
+
+
+    return (
+        <div>
+            {gameRound}
+        </div>
+    );
 }
 
 export default App;
