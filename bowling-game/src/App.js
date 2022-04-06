bdd57f01a43cbb47fbe66bc6f6c1d55df4b88d87
hugh.burgess <hugh.burgess@check24.de>
2022-04-06 13:27:03 +0200

    correct cumulative addition and scoring

--- bowling-game/src/App.js
+++ bowling-game/src/App.js
@@ -1,45 +1,61 @@
 import './App.css';
+import {GiBowlingPin} from "react-icons/gi";
+import {ImCross} from "react-icons/im";
+import {BsFillSquareFill, BsSlashLg} from "react-icons/bs";
 
 function BowlingGame() {
 
     const arrayOfRounds = {
         round: [],
         firstScore: Number,
-        secondScore: Number,
         totalScorePerRound: [],
         cumulatedScore: [],
+        maxFromFirstScoreArray: [],
+        randLeftoverAttemptArray: [],
     };
 
     for (let i = 0; i <= 9; i++) {
         arrayOfRounds.round.push(i);
         arrayOfRounds.firstScore = Array.from({length: 10}, () => Math.floor(Math.random() * 10));
-        arrayOfRounds.secondScore = Array.from({length: 10}, () => Math.floor(Math.random() * 10));
+
+        const maxFromFirstScore = Math.abs(arrayOfRounds.firstScore[i]  - 10);
+        let randLeftoverAttempt = Math.random();
+        randLeftoverAttempt = Math.floor( randLeftoverAttempt * maxFromFirstScore);
+        arrayOfRounds.maxFromFirstScoreArray.push(maxFromFirstScore);
+        arrayOfRounds.randLeftoverAttemptArray.push(randLeftoverAttempt);
     }
-    arrayOfRounds.totalScorePerRound = arrayOfRounds.firstScore.map(function (num, idx) {
-        return num + arrayOfRounds.secondScore[idx];
-    });
-
-    const cumulativeSum = (sum => value => sum += value)(0);
-
-    arrayOfRounds.cumulatedScore = arrayOfRounds.totalScorePerRound.map(cumulativeSum);
 
     return (
         <div>
             <table>
                 <tr>
                     {arrayOfRounds.round.map((i, index) => {
+
+                        const maxFromFirstScore = Math.abs(arrayOfRounds.firstScore[i]  - 10);
+                        let randLeftoverAttempt = Math.random();
+                        randLeftoverAttempt = Math.floor( randLeftoverAttempt * maxFromFirstScore);
+                        arrayOfRounds.maxFromFirstScoreArray.push(maxFromFirstScore);
+                        arrayOfRounds.randLeftoverAttemptArray.push(randLeftoverAttempt);
+                        arrayOfRounds.totalScorePerRound.push(arrayOfRounds.firstScore[i] + randLeftoverAttempt);
+
+                        const cumulativeSum = (sum => value => sum += value)(0);
+
+                        arrayOfRounds.cumulatedScore = arrayOfRounds.totalScorePerRound.map(cumulativeSum);
+
+                        const pin = arrayOfRounds.firstScore[i] === 9 && randLeftoverAttempt < 10;
+                        const blank = arrayOfRounds.firstScore[i] === 1 && randLeftoverAttempt === 1;
+                        const strike = arrayOfRounds.firstScore[i] === 10 && randLeftoverAttempt === 1;
+                        const spare = arrayOfRounds.firstScore[i] + randLeftoverAttempt === 10;
                         return (
                             <th key={index}>
-                                {arrayOfRounds.firstScore[i]} · {
-                                arrayOfRounds.firstScore[i] === 9 && arrayOfRounds.secondScore[i] < 10
-                                ? 'P'
-                                : arrayOfRounds.firstScore[i] === 1 && arrayOfRounds.secondScore[i] === 1
-                                ? ''
-                                : arrayOfRounds.firstScore[i] === 10 && arrayOfRounds.secondScore[i] === 1
-                                ? 'X'
-                                : arrayOfRounds.firstScore[i] + arrayOfRounds.secondScore[i] === 10
-                                ? '/'
-                                : arrayOfRounds.secondScore[i]}
+                                {arrayOfRounds.firstScore[i]} ·
+                                {pin ? <GiBowlingPin/>
+                                : blank ? <BsFillSquareFill/>
+                                : strike ? <ImCross/>
+                                : spare ? <BsSlashLg/>
+                                : randLeftoverAttempt}
+
+                                {arrayOfRounds.round[i] === 9 && (strike || spare) && 'third'}
                             </th>
                         )
                     })}
